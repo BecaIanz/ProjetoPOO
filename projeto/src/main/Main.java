1e9c22a44421136b549e355703d1be8ea766a98d
BecaIanz <silveiraianzr@gmail.com>
2026-06-14 11:13:56 -0300

    Trabalho Somativa 1

--- projeto/src/main/Main.java
+++ projeto/src/main/Main.java
@@ -1,14 +1,22 @@
 package main;
 
+import modelo.Financiamento;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args){
-        util.InterfaceUsuario user = new util.InterfaceUsuario();
-        double valorImovel = user.valorImovel();
-        int prazoFinanciamento = user.prazoFinanciamento();
-        double taxaJuros = user.taxaJuros();
+        ArrayList<Financiamento> financiamentos = new ArrayList<>();
 
-        modelo.Financiamento financiamento = new modelo.Financiamento(valorImovel, prazoFinanciamento, taxaJuros);
+        financiamentos.add(new Financiamento(200000, 10, 0.15));
+        financiamentos.add(new Financiamento(300000, 16, 0.12));
+        financiamentos.add(new Financiamento(230000, 11, 0.14));
+        financiamentos.add(new Financiamento(450000, 22, 0.10));
 
-        financiamento.mostrarDados();
+        int i = 1;
+        for(Financiamento fin : financiamentos){
+            System.out.println("\n------------------------------\nFinanciamento " + i + "\nValor do imóvel: R$" + String.format("%.2f", fin.getValorImovel())  + "\nValor do Financiamento: R$" +  String.format("%.2f", fin.calcTotalPagamento()));
+            i++;
+        }
     }
 }
